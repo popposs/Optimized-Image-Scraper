4b422e33ca48a2c4e1ce262cb6092d6a375cf254
Josh Chang <jchang82@illinois.edu>
2018-08-14 11:50:25 -0500

    update redis

--- src/cache.py
+++ src/cache.py
@@ -6,5 +6,5 @@ _host = options['host']
 _port = options['port']
 _db = options['db']
 
-cache_client = redis.StrictRedis(host=_host, port=_port,  db=_db)
+cache_client = redis.StrictRedis(host=_host, port=_port, db=_db, charset="utf-8", decode_responses=True)
 
--- src/reddit.py
+++ src/reddit.py
@@ -5,24 +5,22 @@ import time
 
 reddit = get_reddit()
 
-
-def check_cached(val):
-	return val.decode('utf-8') if val else None
-
-
 def get_top_posts():
 	global reddit
 	posts = {}
 
 	for channel in CHANNELS:
 		urls = cache_client.get(channel)
-		urls = check_cached(urls)
 
 		if urls is None:
 			submissions = reddit.subreddit(channel).hot(limit=10)
 			urls = [ s.url for s in submissions ]
 			cache_client.set(channel, urls, ex=60) # cache for 1 minute
-		
+
 		posts[channel] = urls
 	
 	return posts
+
+if __name__ == '__main__':
+	print(get_top_posts())
+
