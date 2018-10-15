3d5f4df6b5672f34e74796a490d146ed1a6f5378
popposs <josh.chang.28@gmail.com>
2018-10-14 23:32:16 -0500

    gunicorn

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,6 @@ src/utils/wait-for-it -t 10 cache:6379
 src/utils/wait-for-it -t 10 db:5432
 
 echo "Running backend!"
-python3 src/app.py
-curl -i http://localhost:5000/
+#python3 src/app.py
+gunicorn app:app -w 4 -b :5090
+#curl -i http://localhost:5000/
